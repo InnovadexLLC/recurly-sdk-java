743b234d8ddad4e55a9393b8b5078304e0018d7e
gaurav-chawla <gaurav.chawla@ul.com>
2014-12-22 22:31:37 -0800

    Added for billing

---
+++ src/main/java/com/sciul/recurly/service/SubscriptionsService.java
@@ -0,0 +1,53 @@
+package com.sciul.recurly.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpMethod;
+import org.springframework.stereotype.Service;
+
+import com.sciul.recurly.helper.BillingConstants;
+import com.sciul.recurly.helper.BillingHelper;
+import com.sciul.recurly.model.Subscription;
+
+@Service
+public class SubscriptionsService extends AbsctractService {
+
+  @SuppressWarnings("unused")
+  private static Logger logger = LoggerFactory.getLogger(SubscriptionsService.class);
+
+  @Autowired
+  private BillingHelper billingHelper;
+
+  public Subscription createSubscription(Subscription subscription) {
+    return call(BillingConstants.RecurlyApiPath.SUBSCRIPTIONS.toString(), subscription, Subscription.class,
+          HttpMethod.POST);
+  }
+
+  public Subscription createSubscriptionWithManualInvoice(Subscription subscription, Integer netTerms, String poNumber) {
+    billingHelper.setSubscriptionWithManualInvoice(subscription, netTerms, poNumber);
+    Subscription s = createSubscription(subscription);
+    return s;
+  }
+
+  public Subscription createSubscriptionWithAddOn(Subscription subscription,
+        String addOnCode,
+        Long amountInCents,
+        Integer quantity) {
+    billingHelper.setSubscriptionWithAddOn(subscription, addOnCode, amountInCents, quantity);
+    Subscription s = createSubscription(subscription);
+    return s;
+  }
+
+  public Subscription createManualInvoiceSubscriptionWithAddon(Subscription subscription,
+        Integer netTerms,
+        String poNumber,
+        String addOnCode,
+        Long addOnAmountInCents,
+        Integer quantity) {
+    billingHelper.setSubscriptionWithManualInvoice(subscription, netTerms, poNumber);
+    billingHelper.setSubscriptionWithAddOn(subscription, addOnCode, addOnAmountInCents, quantity);
+    Subscription s = createSubscription(subscription);
+    return s;
+  }
+}
